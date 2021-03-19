728b1575d1e68832ea5907e03dfc9cc43a9f5fbc
Hung Quang <54548817+hungbk99@users.noreply.github.com>
2021-03-19 17:30:20 +0700

    Create apb_types.sv

---
+++ uvm/apb_types.sv
@@ -0,0 +1,10 @@
+//=================================================================
+// Class Description: Data items for APB UVC
+// Project Name:	    renas mcu
+// Ho Chi Minh University of Technology
+// Email: 			quanghungbk1999@gmail.com  
+// Version  Date        Author    Description
+// v0.0     18.03.2021  hungbk99  First Creation  
+//=================================================================
+
+typedef enum bit {APB_READ, APB_WRITE} apb_direction_enum;
