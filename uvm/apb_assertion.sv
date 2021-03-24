fb490e6b85afd95933d7228dbc1e575c7aaa48d1
Hung Quang <54548817+hungbk99@users.noreply.github.com>
2021-03-24 16:55:17 +0700

    Update apb_assertion.sv

--- uvm/apb_assertion.sv
+++ uvm/apb_assertion.sv
@@ -1,55 +1,42 @@
-//Property: PADDR must not be X or Z when PSEL is asserted
-paddr_unknown: assert property(
-                @(posedge pclk)
-                disable iff(!check_enable)    
-                (psel == 0 or !$isunknown(paddr))
-                else $error("PADDR went to X or Z when PSEL is asserted");
-);
+always @(posedge pclk) begin
+    //Property: PADDR must not be X or Z when PSEL is asserted
+    paddr_unknown: assert 
+                    (!((psel == 0) || !$isunknown(paddr) || (check_enable == 0)))
+                      $error("PADDR went to X or Z when PSEL is asserted");
+    
+    //Property: PWRITE must not be X or Z when PSEL is asserted
+    pwrite_unknown: assert 
 
-//Property: PWRITE must not be X or Z when PSEL is asserted
-pwrite_unknown: assert property(
-                @(posedge pclk)
-                disable iff(!check_enable)
-                (psel == 0 or !$isunknown(pwrite))
-                else $error("PWRITE went to X or Z when PSEL is asserted");
-);
+                    (!((psel == 0) || !$isunknown(pwrite) || (check_enable == 0)))
+                      $error("PWRITE went to X or Z when PSEL is asserted");
+    
+    //Property: PWDATA must not be X or Z during a write transaction
+    pwdata_unknown: assert 
 
-//Property: PWDATA must not be X or Z during a write transaction
-pwdata_unknown: assert property(
-                @(posedge pclk)
-                disable iff(!check_enable)
-                (psel == 0 or pwrite == 0 or !$isunknown(pwdata))
-                else $error("PWDATA went to X or Z during a write transaction");
-);
+                    (!((psel == 0) || (pwrite == 0) || !$isunknown(pwdata) || (check_enable == 0)))
+                      $error("PWDATA went to X or Z during a write transaction");
+    
+    //Property: PRDATA must not be X or Z during a read transaction
+    prdata_unknown: assert 
 
-//Property: PRDATA must not be X or Z during a read transaction
-prdata_unknown: assert property(
-                @(posedge pclk)
-                disable iff(!check_enable)
-                (psel == 0 or pready == 0 or pwrite == 1 or !$isunknown(prdata))
-                else $error("PRDATA went to X or Z during a read transaction");
-);
+                    (!((psel == 0) || (pready == 0) || (pwrite == 1) || !$isunknown(prdata) || (check_enable == 0)))
+                      $error("PRDATA went to X or Z during a read transaction");
+    
+    //Property: PENABLE must not be X or Z
+    penable_unknown: assert 
 
-//Property: PENABLE must not be X or Z
-penable_unknown: assert property(
-                 @(posedge pclk)
-                 disable iff(!check_enable)
-                 ($isunknown(penable))
-                 else $error("PENABLE went to X or Z");
-);
+                     (($isunknown(penable)) && (check_enable == 1))
+                       $error("PENABLE went to X or Z");
+    
+    //Property: PSEL must not be X or Z
+    psel_unknown: assert 
 
-//Property: PSEL must not be X or Z
-psel_unknown: assert property(
-                @(posedge pclk) 
-                disable iff(!check_enable)
-                (!$isunknown(psel))
-                else $error("PSEL went to X or Z");
-);
+                    ($isunknown(psel) && (check_enable == 1))
+                      $error("PSEL went to X or Z");
+    
+    //Property: PSLVERR must not be X or Z
+    pslverr_unknow: assert 
 
-//Property: PSLVERR must not be X or Z
-pslverr_unknow: assert property(
-                @(posedge pclk)
-                disable iff(!check_enable)
-                (psel == 0 or pready == 0 or !$isunknown(pslverr))
-                else $error("PSLVERR went to X or Z);
-); 
+                    (!((psel == 0) || (pready == 0) || !$isunknown(pslverr)))
+                      $error("PSLVERR went to X or Z");
+end 
